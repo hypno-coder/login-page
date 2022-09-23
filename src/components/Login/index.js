3a53dc7a16cb6c1b814176dba83b06c40d30f677
hypno-coder <davidov.artur.job@gmail.com>
2022-09-23 17:11:32 +0300

    managed component complete

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 
 const Login = () => {
+  const [name, setName] = useState('')
+  const [pass, setPass] = useState('')
 
   return (
     <form action="" class="postcard">
       <div class="form-row">
-        <label for="name">имя</label><input type="text" id="name" required />
+        <label for="name">имя</label>
+        <input value={name} onChange={(e) => setName(e.currentTarget.value)} type="text" id="name" required />
       </div>
       <div class="form-row">
-        <label for="password">пароль</label><input type="password" id="password" required />
+        <label for="password">пароль</label>
+        <input value={pass} onChange={(e) => setPass(e.currentTarget.value)} type="password" id="password" required />
       </div>
       <div class="form-row">
-        <input type="submit" value="Войти" />
+        <input onClick={() => alert(`${name} / ${pass}`)} type="submit" value="Войти" />
       </div>
     </form>
   )
