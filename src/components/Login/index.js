109741be176d0d69806f3680f04b910c1b2d488e
Artur Davidov <davidov.artur.job@gmail.com>
2022-09-23 18:50:14 +0300

    auth requset complete (#5)

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,22 +1,35 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 import './index.css'
 
 const Login = () => {
   const [name, setName] = useState('')
   const [pass, setPass] = useState('')
 
+  const authFetchHandler = async () => {
+    const url = 'http://neurodoc.online/api/api/authenticate'
+    axios.post(url, {
+      "username": "testuser@tt.ru",
+      "password": "TPipZn2h",
+    }).then(resp => {
+      console.log("Success ========>", resp)
+    }).catch(error => {
+      console.log("Success ========>", error);
+    })
+  }
+
   return (
-    <form action="" class="postcard">
-      <div class="form-row">
-        <label for="name">имя</label>
+    <form action="" className="postcard">
+      <div className="form-row">
+        <label htmlFor="name">имя</label>
         <input value={name} onChange={(e) => setName(e.currentTarget.value)} type="text" id="name" required />
       </div>
-      <div class="form-row">
-        <label for="password">пароль</label>
+      <div className="form-row">
+        <label htmlFor="password">пароль</label>
         <input value={pass} onChange={(e) => setPass(e.currentTarget.value)} type="password" id="password" required />
       </div>
-      <div class="form-row">
-        <input onClick={() => alert(`${name} / ${pass}`)} type="submit" value="Войти" />
+      <div className="form-row">
+        <input onClick={authFetchHandler} type="submit" value="Войти" />
       </div>
     </form>
   )
