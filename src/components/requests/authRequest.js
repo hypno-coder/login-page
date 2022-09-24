54cbddb74bc0546a625dd2ba4d0afb7e61c430eb
Artur Davidov <davidov.artur.job@gmail.com>
2022-09-24 16:16:24 +0300

    fix auth request (#8)

--- src/components/requests/authRequest.js
+++ src/components/requests/authRequest.js
@@ -1,19 +1,20 @@
 import axios from 'axios'
 import { toast } from 'react-toastify';
 
-const authRequest = async (name, pass) => {
+
+const authRequest = (name, pass) => {
   const url = '/api/authenticate'
   const authData = {
     "username": name,
     "password": pass,
   }
-  axios.post(url, { ...authData })
-    .then(resp => {
-      localStorage.setItem('resp', JSON.stringify(resp.data))
-      toast.success("УСПЕХ")
-    }).catch(error => {
-      toast.warn(error.message)
-    })
+  axios.post(url, { ...authData }
+  ).then(resp => {
+    localStorage.setItem('resp', JSON.stringify(resp.data))
+    toast.success("УСПЕХ")
+  }).catch(error => {
+    toast.warn(error.message)
+  })
 }
 
 export default authRequest
