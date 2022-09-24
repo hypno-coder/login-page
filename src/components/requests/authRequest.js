7a060ab1ce36d4311cc19034299b0eb350ce5f01
Artur Davidov <davidov.artur.job@gmail.com>
2022-09-24 13:59:20 +0300

    toast complete (#7)

---
+++ src/components/requests/authRequest.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import { toast } from 'react-toastify';
+
+const authRequest = async (name, pass) => {
+  const url = '/api/authenticate'
+  const authData = {
+    "username": name,
+    "password": pass,
+  }
+  axios.post(url, { ...authData })
+    .then(resp => {
+      localStorage.setItem('resp', JSON.stringify(resp.data))
+      toast.success("УСПЕХ")
+    }).catch(error => {
+      toast.warn(error.message)
+    })
+}
+
+export default authRequest
